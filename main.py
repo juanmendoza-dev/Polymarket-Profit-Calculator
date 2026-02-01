8ac4b2690a0f14c98733095f15a2588a7472d9e8
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-01 00:45:31 -0500

    "Yes" or "no" logic added!

--- main.py
+++
@@ -1,6 +0,0 @@
-import time 
-from colorama import Fore
-import math 
-
-capital = input(f"{Fore.LIGHTMAGENTA_EX} Initial {Fore.LIGHTGREEN_EX}Capital?")
-yes_no_contract = input('Purchased "Yes" or "No" Contract?')
