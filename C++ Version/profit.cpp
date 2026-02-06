33295c868e420a85b2913cfffe2563eaa6669066
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-06 08:18:13 -0500

    Added "No" Logic + Error Logic

--- C++ Version/profit.cpp
+++ C++ Version/profit.cpp
@@ -17,9 +17,17 @@ int main(){
     
     if (yes_no_contract == "Yes"|| yes_no_contract == "YES" || yes_no_contract == "y" || yes_no_contract == "Y"){
        int yes_no_contract = 0; //makes the var into a "Yes" contract
+       
     }
     
-//test commit
+    else if (yes_no_contract == "No" || yes_no_contract == "NO" || yes_no_contract == "n" || yes_no_contract == "N"){
+        int yes_no_contract = 1; //makes the var into a "No contract"
+    }
+
+    else{
+        std::cout << "Invaild Please Try again!";
+    }
+
 
 
     return 0; //goodbye!
