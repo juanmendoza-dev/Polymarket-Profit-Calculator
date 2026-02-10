08fad51c8d99bc27d7aa9399348f36fe6f2b7e94
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-10 09:13:23 -0500

    Updated UI (Completed Version)
    This version is fully functional and logs the user input to "selection" var

--- C++ Version/profit.cpp
+++ C++ Version/profit.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 using std::cout;
 
 int main(){
+    //global vars
+    std::string selection;
+
+
+
     //UI launcher 
     cout << "Polymarket/Kalshi Bot" << "\n";
     cout << "Options: " << "\n";
     cout << "1) Risk Analyzer" << "\n";
     cout << "2) Profit Prediction" << "\n";
     cout << "3) Exit" << "\n";
+    cout << "\n"; //newline
+    cout << "Choose Selection: "; //user selection
+    std::cin >> selection; 
+    
+
+
   
 
     return 0;
