c23c559ae025c9bfad20382c6e5135832f2ff398
Juan Mendoza <juanmendoza6159@gmail.com>
2026-02-15 13:50:43 -0500

    Restructured Vars

--- C++ Version/riskAnalyzer.cpp
+++ C++ Version/riskAnalyzer.cpp
@@ -4,27 +4,12 @@
 using std::cout;
 
 void runRiskAnalyzer(){
+   
     //vars
-    double capital;
-    std::string yes_no_contract;
-    double $pershare;
-    
-    //intro
-    cout << "Running risk Analyzer...\n";
-    cout << "\n";
-
-    //capital (step 1)
-    cout << "Captial: ";
-    std::cin >> capital;
-
-    //yes or no contract (step 2 )
-    cout << '"'<< "Yes" << '"' << "or" <<'"' << "No" << '"' << "Contract"; // "Yes" or "No" Contract
-    std::cin >> yes_no_contract;
-
-    //yes or no contract logic (step 2.5)
-    if (yes_no_contract == "Yes" || yes_no_contract == "Y" == yes_no_contract == "YES")
-    
-
+    double capital; // total amount the user wants to spend 
+    std::string trade1;  // f1 will win best picture (example)
+    std::string contractType; //"Yes" or "No"
+    double $perShare; //price per "Yes" or "No" Share
 
 
 
